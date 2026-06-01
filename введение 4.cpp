10636ef74563a9506d1276a2072418e00647ff48
a7300625 <a7300625@gmail.com>
2026-06-02 00:51:00 +0300

    Добавлены структуры для хранения данных

---
+++ введение 4.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #8. GIT\n";
+    cout << "Variant #0. Library Subscription\n";
+    cout << "Author: Sergey Yermochenko\n";
+    return 0;
+}
