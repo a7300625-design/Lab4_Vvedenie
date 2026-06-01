e5028b098a07db79669b8a5bd6e17831aeb8fcd6
a7300625 <a7300625@gmail.com>
2026-06-02 02:21:33 +0300

    Добавлены модульные тесты для 5 варианта

--- process.cpp
+++
@@ -1,5 +0,0 @@
-#include "processing.h"
-
-long long process(InternetSession* arr[], int size, std::string program_name) {
-    return 0; 
-}
--- processing.h
+++
@@ -1,8 +0,0 @@
-#ifndef PROCESSING_H
-#define PROCESSING_H
-#include "internet_protocol.h"
-#include <string>
-
-long long process(InternetSession* arr[], int size, std::string program_name);
-
-#endif
